2e169dd6fc0ceaa2f83a83e328abe80db5c08d31
Joseph east <josepheast@MacBook-Pro-5.local>
2022-08-01 10:53:20 +0100

    option to start from existing generator v3

--- src/train.py
+++ src/train.py
@@ -112,8 +112,10 @@ if __name__ == "__main__":
     generator_number = args['--generator_number']
     # gen = 'generator 
     generator = models.VideoGenerator(n_channels, dim_z_content, dim_z_category, dim_z_motion, video_length)
+    #print(os.path.join("../logs/exercises",generator_number))
     if generator_number > 0:
-        generator = torch.load(os.path.join("../logs/exercises",generator_number),map_location = 'cpu')
+        # generator = torch.load(os.path.join("../logs/exercises",generator_number),map_location = 'cpu') FOR MAC
+        generator = torch.load(os.path.join("../logs/exercises",generator_number))
     image_discriminator = build_discriminator(args['--image_discriminator'], n_channels=n_channels,
                                               use_noise=args['--use_noise'], noise_sigma=float(args['--noise_sigma']))
 
